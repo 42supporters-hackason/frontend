dfea68c64dc992e4a734a5af19349d0a1ad87c9c
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-12-24 15:40:38 +0900

    add button

--- src/components/pages/ApplyDetail.tsx
+++ src/components/pages/ApplyDetail.tsx
@@ -3,8 +3,17 @@ import { ApplyDetailCards } from '../organisms/ApplyDetailCards'
 import { Header } from '../organisms/Header'
 import Grid from "@mui/material/Grid"
 import { UserDetailCard } from '../organisms/UserDetailCard'
+import { Button } from '@mui/material'
+import styled from "styled-components"
+import { useNavigate } from "react-router-dom"
 
 export const ApplyDetail: VFC = () => {
+	const navigate = useNavigate()
+
+	const onClickApplyNavigator = () => {
+		navigate('/')
+	}
+
 	return (
 		<>
 			<Header />
@@ -14,9 +23,16 @@ export const ApplyDetail: VFC = () => {
 				</Grid>
 				<Grid xs={4}>
 					<UserDetailCard />
+					<ButtonWrapper>
+						<Button variant="outlined" onClick={onClickApplyNavigator}>この募集にApplyする</Button>
+					</ButtonWrapper>
 				</Grid>
 			</Grid>
 		</>
 	)
 }
 
+const ButtonWrapper = styled.div`
+	text-align: center;
+	margin-top: 20px;
+`
